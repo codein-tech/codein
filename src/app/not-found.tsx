47a2497313b3bd6a463686b3244cb98877257d16
codein-tech <codeinumb@gmail.com>
2026-05-07 15:14:29 +0700

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
 import { Code2, Home, ArrowLeft } from "lucide-react";
+import Image from "next/image";
 
 export default function NotFound() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-white dark:bg-dark-900 px-4 text-center">
-      <div className="w-16 h-16 bg-gradient-to-br from-blue-600 to-blue-400 rounded-2xl flex items-center justify-center mb-6 shadow-xl shadow-blue-500/20">
-        <Code2 className="w-8 h-8 text-white" />
+      <div className="w-16 h-16 bg-white rounded-2xl flex items-center justify-center mb-6 shadow-xl >
+                              <Image
+                        src="/images/codein.png"
+                        alt="CodeIn Logo"
+                        width={200}
+                        height={200}
+                        className="object-contain rounded-full"
+                      />
       </div>
       <h1 className="font-display font-bold text-6xl text-gray-900 dark:text-white mb-4">404</h1>
       <h2 className="font-display font-semibold text-xl text-gray-700 dark:text-gray-300 mb-2">Halaman Tidak Ditemukan</h2>
