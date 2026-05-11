200a5f07f4a55203743d997b96868811a7b45b0f
codein-tech <codeinumb@gmail.com>
2026-05-11 22:30:24 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,17 +4,40 @@ import { ThemeProvider } from "@/components/ui/ThemeProvider";
 import { ToastProvider } from "@/components/ui/Toast";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://codein-umb.vercel.app"),
+
   title: {
     default: "CodeIn — Komunitas IT Universitas Muhammadiyah Bengkulu",
     template: "%s | CodeIn",
   },
+
   description:
     "Komunitas belajar, berbagi, dan berkolaborasi di bidang teknologi dan programming untuk mahasiswa Universitas Muhammadiyah Bengkulu.",
+
   keywords: ["CodeIn", "komunitas IT", "programming", "UMB", "Bengkulu"],
+
   openGraph: {
     title: "CodeIn",
     description: "Komunitas IT Universitas Muhammadiyah Bengkulu",
+    url: "https://codein-umb.vercel.app",
+    siteName: "CodeIn",
     type: "website",
+
+    images: [
+      {
+        url: "/images/deffault.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "CodeIn",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "CodeIn",
+    description: "Komunitas IT Universitas Muhammadiyah Bengkulu",
+    images: ["/images/deffault.jpeg"],
   },
 };
 
