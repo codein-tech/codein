8916f937e0c821d386da6d6729c026e5593a1a80
codein-tech <codeinumb@gmail.com>
2026-05-11 23:33:24 +0700

    Create head.tsx

---
+++ src/app/articles/[id]/head.tsx
@@ -0,0 +1,22 @@
+export default function Head() {
+  return (
+    <>
+      <title>TEST ARTICLE</title>
+
+      <meta
+        property="og:title"
+        content="TEST ARTICLE"
+      />
+
+      <meta
+        property="og:description"
+        content="TEST DESCRIPTION"
+      />
+
+      <meta
+        property="og:image"
+        content="https://codein-umb.vercel.app/og/default.jpg"
+      />
+    </>
+  );
+}
